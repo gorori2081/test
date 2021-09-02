1bb8a35b2bf59d05cc9813012457a8eb5252ac87
masahiro <masahirokun2081@gamil.com>
2021-09-02 21:45:13 +0900

    かきくけこ

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const p = document.querySelectorAll('p')[0];
+setTimeout(function(){
+    p.style.color = 'blue';
+},5000);
+function calc(x,y){
+    return x+1+y+2;
+}
+console.log(10+1);
+console.log(11+1);
+console.log(100+1);
+console.log(calc(20,30));
